bab8dd0af554b7739f316e27f68af87687359f97
Bruno Codeman <brunobemfica@gmail.com>
2023-05-09 16:35:46 -0300

    Adicionando arquivos iniciais do projeto

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module br.org.cria.splinkerapp {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens br.org.cria.splinkerapp to javafx.fxml;
+    exports br.org.cria.splinkerapp;
+}
