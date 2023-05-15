9217dabfb178979c578698f9a1cd28b0efc8d99a
Bruno Codeman <brunobemfica@gmail.com>
2023-05-15 19:33:05 -0300

    atualizando visibilidade do pacote

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,10 @@
 module br.org.cria.splinkerapp {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
-    opens br.org.cria.splinkerapp to javafx.fxml;
+    opens br.org.cria.splinkerapp.controllers to javafx.fxml;
+    
     exports br.org.cria.splinkerapp;
 }
