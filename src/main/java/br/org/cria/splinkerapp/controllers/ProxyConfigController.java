6107f782e299974bab38e8f85d7767a8065e5b7d
Bruno Codeman <brunobemfica@gmail.com>
2023-05-19 18:28:29 -0300

    Adicionando navegação

--- src/main/java/br/org/cria/splinkerapp/controllers/ProxyConfigController.java
+++ src/main/java/br/org/cria/splinkerapp/controllers/ProxyConfigController.java
@@ -1,5 +1,6 @@
 package br.org.cria.splinkerapp.controllers;
 
+import br.org.cria.splinkerapp.Router;
 import br.org.cria.splinkerapp.models.ProxyConfiguration;
 import br.org.cria.splinkerapp.services.ProxyConfigService;
 import br.org.cria.splinkerapp.services.interfaces.IProxyConfigService;
@@ -38,7 +39,10 @@ public class ProxyConfigController extends AbstractController{
 
     @FXML
     void onLinkNoProxyClicked(){
-
+        var routeName = "datasource-selection";
+        var width = 320;
+        var height = 240;
+        Router.getInstance().navigateTo(getStage(), routeName, width, height);
     }
     @FXML
     void onButtonSaveClicked(){
