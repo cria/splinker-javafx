484a41621ef6692ca109038d05c7ecfb29ab9506
Bruno Codeman <brunobemfica@gmail.com>
2023-05-22 22:48:22 -0300

    Adicionando esboço de seleção de datasource ODBC

--- src/main/java/br/org/cria/splinkerapp/controllers/CollectionLocalDatabaseController.java
+++ src/main/java/br/org/cria/splinkerapp/controllers/CollectionLocalDatabaseController.java
@@ -1,5 +1,9 @@
 package br.org.cria.splinkerapp.controllers;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Arrays;
 import java.util.List;
 
@@ -22,11 +26,36 @@ public class CollectionLocalDatabaseController extends AbstractController{
     @FXML
     TextField tablenameField;
     @FXML
+    TextField portField;
+    @FXML
     Button saveBtn;
 
 
+
     @FXML
-    void onSaveButtonClicked(){}
+    void onSaveButtonClicked() throws ClassNotFoundException{
+        Class.forName("com.mysql.cj.jdbc.Driver");  
+        String url = "jdbc:mysql://localhost:3306/bruno_testdb";
+        String username = usernameField.getText();
+        String password = passwordField.getText();
+
+        try(Connection connection = DriverManager.getConnection(url, username, password)) {
+            Statement statement = connection.createStatement();
+
+            String sql = "SELECT * FROM %s;".formatted(tablenameField.getText());
+
+            var result = statement.executeQuery(sql);
+            while(result.next()){
+                var name = result.getString("name");
+                var address = result.getString("address");
+                var phone = result.getString("phone");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
     @Override
     protected Pane getPane() { return this.pane; }
     
