912cd2f83242f86589ac9011bf26288b34d15d49
Bruno Codeman <brunobemfica@gmail.com>
2023-05-23 22:52:16 -0300

    Implementando leitura de caminho de arquivo

--- src/main/java/br/org/cria/splinkerapp/controllers/FileSelectionController.java
+++ src/main/java/br/org/cria/splinkerapp/controllers/FileSelectionController.java
@@ -1,23 +1,34 @@
 package br.org.cria.splinkerapp.controllers;
 
+import java.io.File;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
+import javafx.stage.FileChooser;
 
 public class FileSelectionController extends AbstractController{
 
     @FXML
     Pane pane;
     @FXML
+    TextField filePath;
+    @FXML
     Button btnSelectFile;
     @FXML
     Button btnSave;
-    
+    FileChooser fileChooser = new FileChooser();
     @Override
     protected Pane getPane() {return pane;}
     
     @FXML
-    void onButtonSelectFileClicked(){}
+    void onButtonSelectFileClicked(){
+        File file = fileChooser.showOpenDialog(getStage());
+        if (file != null) {
+        filePath.setText(file.getAbsolutePath());
+        }
+    }
     
     @FXML
     void onButtonSaveClicked(){}
