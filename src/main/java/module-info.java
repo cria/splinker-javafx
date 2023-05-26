5286e55590c6e18ab2f405b56533c77c01141e23
Bruno Codeman <brunobemfica@gmail.com>
2023-05-25 23:25:20 -0300

    adicionando bibliotecas ao módulo

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,8 +2,12 @@ module br.org.cria.splinkerapp {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
-
-
+    requires rsync4j.all;
+    requires rsync4j.core;
+    requires rsync4j.windows64;
+    requires processoutput4j;
+    requires jsch;
+    
     opens br.org.cria.splinkerapp.controllers to javafx.fxml;
     
     exports br.org.cria.splinkerapp;
