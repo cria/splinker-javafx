160fa345c8078ab2f2d267ba9e02622cb7d09373
Bruno Codeman <brunobemfica@gmail.com>
2023-05-29 21:29:51 -0300

    limpando código e ligando controller ao fxml

--- src/main/java/br/org/cria/splinkerapp/controllers/HomeController.java
+++ src/main/java/br/org/cria/splinkerapp/controllers/HomeController.java
@@ -4,15 +4,11 @@ import java.net.InetSocketAddress;
 import java.net.Proxy;
 import java.net.ProxySelector;
 import java.net.URI;
-import java.util.Currency;
 import java.util.Iterator;
 import java.util.List;
 
 import com.github.fracpete.processoutput4j.output.CollectingProcessOutput;
-import com.github.fracpete.processoutput4j.output.ConsoleOutputProcessOutput;
 import com.github.fracpete.rsync4j.RSync;
-import com.github.fracpete.rsync4j.Ssh;
-import com.github.fracpete.rsync4j.SshPass;
 import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.Session;
 
@@ -70,47 +66,7 @@ public class HomeController extends AbstractController{
     }
     
     @FXML
-    void onSyncMetadataBtnClicked(){
-
-      try {
-        System.setProperty("java.net.useSystemProxies","true");
-        List<Proxy> l = ProxySelector.getDefault().select(
-                    new URI("http://www.yahoo.com/"));
-
-        for (Iterator<Proxy> iter = l.iterator(); iter.hasNext(); ) {
-
-          // proxy hostname : HTTP
-          // proxy host string: 127.0.0.1
-          // proxy address : null
-          // proxy hostname : 127.0.0.1
-          // proxy port : 8866
-          
-
-            Proxy proxy = iter.next();
-
-            System.out.println("proxy hostname : " + proxy.type());
-
-            InetSocketAddress addr = (InetSocketAddress)proxy.address();
-
-            if(addr == null) {
-
-                System.out.println("No Proxy");
-
-            } else {
-              var user = System.getProperty("http.proxyUser", System.getProperty("https.proxyUser"));
-              var pwd = System.getProperty("http.proxyPassword", System.getProperty("https.proxyPassword"));
-              var port = System.getProperty("http.proxyPassword", System.getProperty("https.proxyPort"));
-              System.out.println("proxy host string: " +addr.getHostString());
-                System.out.println("proxy address : " + addr.getAddress());
-                System.out.println("proxy hostname : " + addr.getHostName());
-                System.out.println("proxy port : " + addr.getPort());
-            }
-        }
-    } catch (Exception e) {
-        e.printStackTrace();
-    }
-
-    }
+    void onSyncMetadataBtnClicked(){ }
 
     @Override
     protected Pane getPane() {
