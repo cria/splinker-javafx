9022158f9741d1c10b822138e1a38309b799a965
Bruno Codeman <brunobemfica@gmail.com>
2023-06-01 11:28:06 -0300

    reorganizando as estruturas de tabelas

--- scripts/sql/create_tables.sql
+++ scripts/sql/create_tables.sql
@@ -1,11 +1,21 @@
-CREATE TABLE IF NOT EXISTS LocalConfiguration (
-  db_password VARCHAR(100) NOT NULL,
-  central_service_uri VARCHAR(100) NOT NULL,
-  central_service_url VARCHAR(100) NOT NULL,
+CREATE TABLE IF NOT EXISTS ProxyConfiguration (
   proxy_username VARCHAR(50) NOT NULL,
   proxy_password VARCHAR(100) NOT NULL,
   proxy_port VARCHAR(10) NOT NULL,
   proxy_address VARCHAR(100) NOT NULL,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL
+)
+
+CREATE TABLE IF NOT EXISTS DbConfiguration (
+  db_password VARCHAR(100) NOT NULL,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL
+)
+
+CREATE TABLE IF NOT EXISTS CentralServiceConfiguration (
+  central_service_uri VARCHAR(100) NOT NULL,
+  central_service_url VARCHAR(100) NOT NULL,
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
   updated_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL
 )
