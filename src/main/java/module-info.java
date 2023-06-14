fbd928b68efe4967a96d94891be9afb8e37ddb9b
Bruno Codeman <brunobemfica@gmail.com>
2023-06-14 17:00:24 -0300

    removendo módulo de dependência removida

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,12 +2,12 @@ module br.org.cria.splinkerapp {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires processoutput4j;
     requires rsync4j.all;
     requires rsync4j.core;
     requires rsync4j.windows64;
-    requires processoutput4j;
-    requires jsch;
-    
+    requires org.apache.commons.lang3;
+        
     opens br.org.cria.splinkerapp.controllers to javafx.fxml;
     
     exports br.org.cria.splinkerapp;
