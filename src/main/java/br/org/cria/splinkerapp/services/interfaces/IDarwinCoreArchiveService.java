a9b4ccd0a9d98eafce074040aee11147c3c48b01
Bruno Codeman <brunobemfica@gmail.com>
2023-07-31 23:43:24 -0300

    Corrigindo interface

--- src/main/java/br/org/cria/splinkerapp/services/interfaces/IDarwinCoreArchiveService.java
+++ src/main/java/br/org/cria/splinkerapp/services/interfaces/IDarwinCoreArchiveService.java
@@ -1,10 +1,12 @@
 package br.org.cria.splinkerapp.services.interfaces;
 
+import br.org.cria.splinkerapp.models.DataSource;
+
 import java.util.List;
 
 public interface IDarwinCoreArchiveService {
     List<String> getDarwinCoreFields();
     boolean generateTXTFile();
     boolean generateZIPFile();
-    boolean readDataFromSource();
+    boolean readDataFromSource(DataSource source);
 }
