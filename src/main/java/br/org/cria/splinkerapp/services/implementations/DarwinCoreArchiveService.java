9b236faf04576aa0fd8ff7c5b2a4af61b2334b69
Bruno Codeman <brunobemfica@gmail.com>
2023-08-07 22:09:01 -0300

    - Implementando transferData - Alterando assinatura dos métodos

--- src/main/java/br/org/cria/splinkerapp/services/implementations/DarwinCoreArchiveService.java
+++ src/main/java/br/org/cria/splinkerapp/services/implementations/DarwinCoreArchiveService.java
@@ -2,74 +2,68 @@ package br.org.cria.splinkerapp.services.implementations;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import br.org.cria.splinkerapp.models.DataSource;
 import br.org.cria.splinkerapp.services.interfaces.IDarwinCoreArchiveService;
+import com.github.perlundq.yajsync.ui.YajsyncClient;
+import javafx.concurrent.Service;
+import javafx.concurrent.Task;
+import org.apache.commons.lang3.time.StopWatch;
 
 public class DarwinCoreArchiveService implements IDarwinCoreArchiveService{
 
     @Override
-    public List<String> getDarwinCoreFields() 
-    {
-        return Arrays.asList("id", "type", "modified", "language", "license", "rightsHolder", 
-        "accessRights", "bibliographicCitation", "references", "institutionID", 
-        "collectionID", "datasetID", "institutionCode", "collectionCode", 
-        "datasetName", "ownerInstitutionCode", "basisOfRecord", "informationWithheld", 
-        "dataGeneralizations", "dynamicProperties", "occurrenceID", "catalogNumber", 
-        "recordNumber", "recordedBy", "individualCount", "sex", "lifeStage", 
-        "reproductiveCondition", "behavior", "establishmentMeans", "occurrenceStatus", 
-        "preparations", "disposition", "associatedMedia", "associatedReferences", 
-        "associatedSequences", "associatedTaxa", "otherCatalogNumbers", 
-        "occurrenceRemarks", "organismID", "previousIdentifications", "organismName", 
-        "organismScope", "associatedOccurrences", "associatedOrganisms", 
-        "hostOrganismID", "recordedByIDs", "samplingProtocol", "samplingEffort", 
-        "sampleSizeValue", "sampleSizeUnit", "fieldNumber", "fieldNotes", 
-        "eventDate", "eventTime", "startDayOfYear", "endDayOfYear", "year", 
-        "month", "day", "verbatimEventDate", "habitat", "samplingStrategy", 
-        "measurementDeterminedBy", "measurementMethod", "measurementRemarks", 
-        "measurementType", "measurementUnit", "measurementValue", 
-        "dissolvedOxygen", "depth", "temperature", "salinity", "ph", "locationID", 
-        "higherGeographyID", "higherGeography", "continent", "waterBody", "islandGroup", 
-        "island", "country", "countryCode", "stateProvince", "county", "municipality", 
-        "locality", "verbatimLocality", "minimumDepthInMeters", "maximumDepthInMeters", 
-        "verbatimDepth", "decimalLatitude", "decimalLongitude", "geodeticDatum", 
-        "coordinateUncertaintyInMeters", "coordinatePrecision", "pointRadiusSpatialFit", 
-        "footprintWKT", "footprintSRS", "footprintSpatialFit", "georeferencedBy", "georeferenceProtocol", 
-        "georeferenceSources", "georeferenceVerificationStatus", "georeferenceRemarks", "identifiedBy", 
-        "dateIdentified", "identificationReferences", "identificationRemarks", "taxonID", "scientificNameID", 
-        "acceptedNameUsageID", "parentNameUsageID", "originalNameUsageID", "nameAccordingToID", "namePublishedInID", 
-        "taxonConceptID", "scientificName", "acceptedNameUsage", "parentNameUsage", "originalNameUsage", "nameAccordingTo", 
-        "namePublishedIn", "namePublishedInYear", "higherClassification", "kingdom", "phylum", "classs", "order", "family", 
-        "genus", "subgenus", "specificEpithet", "infraspecificEpithet", "taxonRank", "verbatimTaxonRank", "vernacularName", 
-        "nomenclaturalCode", "taxonomicStatus", "nomenclaturalStatus", "taxonRemarks");
-
-    }
-
-    @Override
-    public boolean generateTXTFile() {
+    public DarwinCoreArchiveService generateTXTFile() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'generateTXTFile'");
     }
 
     @Override
-    public boolean generateZIPFile() {
+    public DarwinCoreArchiveService generateZIPFile() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'generateZIPFile'");
     }
 
     @Override
-    public boolean readDataFromSource(DataSource source) {
-        //Talvez fazer essa classe ter method chaining
-        //seja mais apropriado.
-        return switch (source.getType()){
-            case MySQL, SQLServer -> false;
-            case PostgreSQL -> false;
-            case dBase -> false;
-            case Excel -> false;
-            case Access -> false;
-            case LibreOfficeCalc -> false;
-            default -> true;
-        };
+    public DarwinCoreArchiveService readDataFromSource(DataSource source) {
+        throw new UnsupportedOperationException("Unimplemented method 'generateZIPFile'");
     }
-    
+
+    @Override
+    public DarwinCoreArchiveService transferData() {
+        try
+        {
+            var port = 10_000;
+            //TODO: Ler source e user dir do BD local
+            var source = "/Users/brunobemfica/Downloads/dwca-tropicosspecimens-v1.124.zip.old.zip";
+            var destination = "bruno@34.68.143.184::meu_modulo";
+            var command = new String[]{"--port=%s".formatted(port), "-r", source, destination};
+            var client = new YajsyncClient();
+            var service = new Service<Void>() {
+
+                @Override
+                protected Task<Void> createTask() {
+                    // Create a Task
+                    return new Task<>() {
+                        @Override
+                        protected Void call() throws Exception {
+                            var session = client.start(command);
+                            return null;
+                        }
+                    };
+                }
+            };
+
+            // Start the Service
+            service.start();
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return this;
+    }
+
 }
