78b9884eab06751c869a26f0ed86c750266665c0
Bruno Codeman <brunobemfica@gmail.com>
2023-09-04 23:09:00 -0300

    Implementando conversão de CSV pra excel

--- src/main/java/br/org/cria/splinkerapp/services/implementations/ExcelFileSourceParser.java
+++ src/main/java/br/org/cria/splinkerapp/services/implementations/ExcelFileSourceParser.java
@@ -1,6 +1,9 @@
 package br.org.cria.splinkerapp.services.implementations;
 
+import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -23,7 +26,9 @@ public class ExcelFileSourceParser extends FileSourceParser{
         this.fileSourcePath = fileSourcePath;
 
         try {
-            workbook = new XSSFWorkbook(new FileInputStream(fileSourcePath));
+            workbook = fileSourcePath.endsWith(".csv") ? 
+                            csvToExcel(fileSourcePath) : 
+                            new XSSFWorkbook(new FileInputStream(fileSourcePath));
 
         } catch (IOException e) {
 
@@ -118,5 +123,48 @@ public class ExcelFileSourceParser extends FileSourceParser{
         } catch (IllegalArgumentException e) {
             e.printStackTrace();
         }
+    }
+
+    protected Workbook csvToExcel(String csvPath) throws FileNotFoundException, IOException {
+        workbook = new XSSFWorkbook();
+        var sheet = workbook.createSheet("spLinker");
+        var reader = new BufferedReader(new FileReader(csvPath));
+        var separator = getCsvSeparator(csvPath);
+        String line;
+        int rowNumber = 0;
+        while ((line = reader.readLine()) != null) {
+            Row row = sheet.createRow(rowNumber++);
+            String[] cells = line.split("%s".formatted(separator));
+            int columnNumber = 0;
+            for (String cellData : cells) {
+                Cell cell = row.createCell(columnNumber++);
+                cell.setCellValue(cellData);
+            }
+        }
+        reader.close();
+        
+        return workbook;
+    }
+
+    String getCsvSeparator(String filePath) throws IOException {
+        String result = null;
+    
+        try (var reader = new BufferedReader(new FileReader(filePath))) 
+        {
+            String firstLine = reader.readLine();
+            var searchList =List.of( ",", ";", "\t", "|", ":" );
+            int lowestIndex = Integer.MAX_VALUE;
+            
+            for (String searchItem : searchList) {
+                int index = firstLine.indexOf(searchItem);
+                if (index != -1 && index < lowestIndex) {
+                    lowestIndex = index;
+                    result = searchItem;
+                }
+            }
+        } catch (Exception e) {
+            throw e;
+        }
+        return result;
     }
 }
