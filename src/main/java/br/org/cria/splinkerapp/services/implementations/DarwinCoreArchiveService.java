f9e324811cbbd79705686af66a13cbfb83dc2317
Bruno Codeman <brunobemfica@gmail.com>
2023-09-20 22:40:26 -0300

    Implementando leitura de dados e geração de arquivo darwin core

--- src/main/java/br/org/cria/splinkerapp/services/implementations/DarwinCoreArchiveService.java
+++ src/main/java/br/org/cria/splinkerapp/services/implementations/DarwinCoreArchiveService.java
@@ -12,7 +12,9 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.List;
@@ -20,52 +22,138 @@ import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class DarwinCoreArchiveService implements IDarwinCoreArchiveService {
-    static String zipFile = "%s/spLinker_dwca.zip".formatted(System.getProperty("user.dir"));
-    static String textFile = "%s/spLinker_dwca.txt".formatted(System.getProperty("user.dir"));
+    String zipFile = "%s/spLinker_dwca.zip".formatted(System.getProperty("user.dir"));
+    String textFile = "%s/spLinker_dwca.txt".formatted(System.getProperty("user.dir"));
+    ResultSet data;
 
     @Override
-    public DarwinCoreArchiveService generateTXTFile() {
-        var file = new File(textFile);
-        if (!file.exists()) {
-            try {
-                file.createNewFile();
-                return this;
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
+    public DarwinCoreArchiveService generateTXTFile() throws IOException, SQLException 
+    {   
+        var path = Path.of(textFile);
+        if (Files.exists(path)) 
+        {
+            Files.delete(path);
         }
-        throw new UnsupportedOperationException("Unimplemented method 'generateTXTFile'");
+        
+        var columnNames = getColumnNames();
+        var rows = getDataSourceRows();
+        var writer = new BufferedWriter(new FileWriter(textFile));
+        writer.write(columnNames);        
+        writer.write(rows);
+        writer.flush();
+        writer.close();
+
+        return this;
+    }
+
+    private String getDataSourceRows() throws SQLException
+    {
+        var dataSourceRows = new StringBuilder();
+        while (data.next()) 
+        {
+            var resultSetMetaData = data.getMetaData();
+            var count = resultSetMetaData.getColumnCount();
+            for (int i = 0; i <= count; i++) 
+            {
+                var content = "%s;".formatted(data.getString(i));
+                dataSourceRows.append(content);
+            }
+    
+             dataSourceRows.append("\n");
+        }
+        return dataSourceRows.toString();
+    }
+
+    private String getColumnNames() throws SQLException
+    {
+        var builder = new StringBuilder();
+        var metaData = data.getMetaData();
+        int count = metaData.getColumnCount();
+        
+        for(int i = 0; i<=count; i++) 
+        {
+           builder.append("%s;".formatted(metaData.getColumnName(i)));
+        }
+        builder.append("\n");
+        
+        return builder.toString();
     }
 
     @Override
-    public DarwinCoreArchiveService generateZIPFile() {
+    public DarwinCoreArchiveService generateZIPFile() 
+    {
 
-        try (ZipOutputStream zipOut = new ZipOutputStream(new FileOutputStream(zipFile))) {
+        try (ZipOutputStream zipOut = new ZipOutputStream(new FileOutputStream(zipFile))) 
+        {
             File fileToZip = new File(textFile);
             zipOut.putNextEntry(new ZipEntry(fileToZip.getName()));
             Files.copy(fileToZip.toPath(), zipOut);
             return this;
-        } catch (Exception e) {
+        } 
+        catch (Exception e) 
+        {
             throw new RuntimeException(e);
         }
     }
 
     @Override
-    public DarwinCoreArchiveService readDataFromSource(DataSource source)
-            throws SQLException, IOException, URISyntaxException {
+    public DarwinCoreArchiveService readDataFromSource(DataSource source)throws SQLException, IOException, URISyntaxException
+    {
+        var command = getQueryCommandFromAPI();
+        var uri = source.getConnectionString();
+        var conn = DriverManager.getConnection(uri);
+        var statement = conn.createStatement();
+        this.data = statement.executeQuery(command);
+        
+        return this;
+    }
+
+    @Override
+    public Service<Void> transferData() 
+    {
+        // TODO: Ler map.dat e db.dat das coleções.
+        // Esses arquivos são combinados para montar o select dos dados
+        // nas fontes de dados das coleções (DB, planilhas, Brahms, etc)
+        // O que não for planilha, é banco sempre
+        var port = ConfigurationData.getRSyncPort();
+        // var source =
+        // "%s/splinker_dwca.zip".formatted(System.getProperty("user.dir"));
+        var destination = ConfigurationData.getTransferDataDestination();
+        var command = new String[] { "--port=%s".formatted(port), "-r", this.zipFile, destination };
+        var client = new YajsyncClient();
+        return new Service<Void>() {
+            @Override
+            protected Task<Void> createTask() 
+            {
+                return new Task<>() {
+                    @Override
+                    protected Void call() throws Exception 
+                    {
+                        var session = client.start(command);
+                        return null;
+                    }
+                };
+            }
+        };
+
+    }
+
+    private String getQueryCommandFromAPI() throws IOException, URISyntaxException 
+    {
         String line;
         var command = "";
         var urlConn = new URI("http://localhost:8000/api/get_query").toURL();
         var response = new StringBuffer();
         var connection = (HttpURLConnection) urlConn.openConnection();
         connection.setRequestMethod("GET");
-        
         var reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-        while ((line = reader.readLine()) != null) {
+        
+        while ((line = reader.readLine()) != null) 
+        {
             response.append(line);
         }
+        
         reader.close();
-        // Disconnect the connection
         connection.disconnect();
 
         HashMap<String, List<Double>> json = new Gson().fromJson(response.toString(), HashMap.class);
@@ -74,51 +162,6 @@ public class DarwinCoreArchiveService implements IDarwinCoreArchiveService {
         var out = new ByteArrayOutputStream();
         strValues.forEach((e) -> out.write(e.byteValue()));
         command = new String(out.toByteArray(), StandardCharsets.UTF_8);
-
-        switch (source.getType()) {
-            case MySQL:
-            case PostgreSQL:
-            case SQLServer:
-                break;
-            default:
-                var uri = "jdbc:sqlite:splinker.db";
-                var conn = DriverManager.getConnection(uri);
-                var statement = conn.createStatement();
-                var resulSet = statement.executeQuery(command);
-                while (resulSet.next()) {
-                    var content = resulSet.getString(4);
-                    System.out.println(content);
-                }
-                break;
-        }
-        return this;
-    }
-
-    @Override
-    public Service transferData() {
-        // TODO: Ler map.dat e db.dat das coleções.
-        // Esses arquivos são combinados para montar o select dos dados
-        // nas fontes de dados das coleções (DB, planilhas, Brahms, etc)
-        // O que não for planilha, é banco sempre
-        var port = ConfigurationData.getRSyncPort();
-        var source = "/Users/brunobemfica/Downloads/dummy_data.dart";
-        // var source =
-        // "%s/splinker_dwca.zip".formatted(System.getProperty("user.dir"));
-        var destination = ConfigurationData.getTransferDataDestination();
-        var command = new String[] { "--port=%s".formatted(port), "-r", source, destination };
-        var client = new YajsyncClient();
-        return new Service<Void>() {
-            @Override
-            protected Task<Void> createTask() {
-                return new Task<>() {
-                    @Override
-                    protected Void call() throws Exception {
-                        var session = client.start(command);
-                        return null;
-                    }
-                };
-            }
-        };
-
+        return command;
     }
 }
