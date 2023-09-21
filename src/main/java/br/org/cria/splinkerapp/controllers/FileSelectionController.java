e21412d49c62c0d026e5ee469a62d9d8b6d22dc9
Bruno Codeman <brunobemfica@gmail.com>
2023-09-20 23:50:52 -0300

    Implementando ETL de arquivos

--- src/main/java/br/org/cria/splinkerapp/controllers/FileSelectionController.java
+++ src/main/java/br/org/cria/splinkerapp/controllers/FileSelectionController.java
@@ -2,8 +2,13 @@ package br.org.cria.splinkerapp.controllers;
 
 import java.io.File;
 
+import br.org.cria.splinkerapp.services.implementations.DarwinCoreArchiveService;
+import br.org.cria.splinkerapp.services.implementations.ExcelFileSourceParser;
 import br.org.cria.splinkerapp.Router;
+import br.org.cria.splinkerapp.models.DataSource;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
@@ -24,21 +29,40 @@ public class FileSelectionController extends AbstractController{
     protected Pane getPane() {return pane;}
     
     @FXML
-    void onButtonSelectFileClicked(){
+    void onButtonSelectFileClicked() 
+    {
         File file = fileChooser.showOpenDialog(getStage());
         if (file != null) 
         {
             filePath.setText(file.getAbsolutePath());
+            try
+            {
+                var sourceParser = new ExcelFileSourceParser(file.getAbsolutePath());
+                var dwcManager = new DarwinCoreArchiveService();
+                sourceParser.createTableBasedOnSheet();
+                sourceParser.insertDataIntoTable();
+                dwcManager.readDataFromSource(new DataSource())
+                .generateTXTFile()
+                .generateZIPFile()
+                .transferData();
+                
+            }
+            catch (Exception ex)
+            {
+                    Alert dialog = new Alert(AlertType.ERROR);
+                    dialog.setTitle("Erro");
+                    dialog.setContentText(ex.getMessage());
+                    dialog.show();
+            }
         }
     }
     
     @FXML
-    void onButtonSaveClicked(){
+    void onButtonSaveClicked()
+    {
         var routeName = "home";
         var width = 350;
         var height = 2540;
         Router.getInstance().navigateTo(getStage(), routeName,width, height);
     }
-    
-
 }
