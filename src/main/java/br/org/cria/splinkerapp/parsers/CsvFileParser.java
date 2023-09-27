82f69720e11574a4e513e8a75a15955517d0f579
Bruno Codeman <brunobemfica@gmail.com>
2023-09-27 00:22:26 -0300

    Refatorando leitura de CSV

--- src/main/java/br/org/cria/splinkerapp/parsers/CsvFileParser.java
+++ src/main/java/br/org/cria/splinkerapp/parsers/CsvFileParser.java
@@ -1,23 +1,28 @@
 package br.org.cria.splinkerapp.parsers;
 
 import java.io.FileReader;
-import java.sql.SQLException;
+import com.opencsv.CSVParser;
+import com.opencsv.CSVParserBuilder;
 import com.opencsv.CSVReader;
-import com.opencsv.exceptions.CsvValidationException;
-
+import com.opencsv.CSVReaderBuilder;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
-public class CsvFileParser extends FileParser {
+public class CsvFileParser extends FileParser 
+{
 
     CSVReader  reader;
     String limiter;
-    String[] columnRow;
+    List<String> columns;
+    String filePath;
     public CsvFileParser(String filePath) throws Exception
     {
+        this.filePath = filePath;
         reader = new CSVReader(new FileReader(filePath));
-        columnRow = reader.readNext();
+        var columnRow = reader.readNext();
+        limiter = getCsvSeparator(columnRow[0]);
+        columns = getRowAsStringList(columnRow[0], columnRow.length).stream().map((e)-> makeColumnName(normalizeString(e))).toList();
     }
     
     String getCsvSeparator(String firstLine) throws IOException 
@@ -40,38 +45,44 @@ public class CsvFileParser extends FileParser {
     @Override
     public List<String> getRowAsStringList(Object row, int numberOfColumns) 
     {
-        String[] fullRow = (String[]) row;
-        return Arrays.asList(fullRow);
+        var fullRow = (String) row;
+        var splittedRows = fullRow.split(limiter);
+        return Arrays.asList(splittedRows);
     }
-
     @Override
     protected String buildCreateTableCommand() 
     {
         var tableName = getTableName();
-        List<String> columns = getRowAsStringList(columnRow, columnRow.length).stream().map((e)-> "%s VARCHAR(1),".formatted(makeColumnName(e))).toList();
-        var columnNames = String.join(",", columns);
-        var command = "CREATE TABLE IF NOT EXISTS %s (%s)".formatted(tableName,columnNames);
+        var columnNames = String.join(",", columns.stream().map((e)-> "%s VARCHAR(1)".formatted(e)).toList());
+        var command = createTableCommand.formatted(tableName,columnNames).replace(",)"," );");
         return command;
     }
 
     @Override
-    public void insertDataIntoTable() throws SQLException, CsvValidationException, IOException {
-        String[] nextLine;
+    public void insertDataIntoTable() throws Exception {
         var conn = getConnection();
         var tableName = getTableName();
-        var valuesStr= makeValueString(columnRow.length);
-        var commandBase = "INSERT INTO %s (%s) VALUES (%s);";
-        var columns = getRowAsStringList(columnRow, columnRow.length).stream().map((e)->makeColumnName(e)).toList();
+        var valuesStr= makeValueString(columns.size());
         var columnNames = String.join(",", columns);
-        while ((nextLine = reader.readNext()) != null) 
-        {
-            var command = commandBase.formatted(tableName, columnNames, valuesStr).replace(",)", ")");
-            var statement = conn.prepareStatement(command);
-            for (int i = 0; i < columnRow.length; i++) 
+        CSVParser csvParser = new CSVParserBuilder().withSeparator(limiter.toCharArray()[0]).build(); // custom separator
+        try(CSVReader reader = new CSVReaderBuilder(
+                new FileReader(filePath))
+                .withCSVParser(csvParser)   // custom CSV parser
+                .withSkipLines(1)           // skip the first line, header info
+                .build()){
+            List<String[]> rows = reader.readAll();
+            var rowCount = rows.size();
+            var columnCount = columns.size();
+            for (int i = 0; i < rowCount; i++) 
             {
-                statement.setString(i + 1, nextLine[i]);
+                var command = insertIntoCommand.formatted(tableName, columnNames, valuesStr).replace(",)", ")");
+                var statement = conn.prepareStatement(command);
+                for (int j = 0; j < columnCount; j++) 
+                {
+                    statement.setString(j + 1, rows.get(i)[j]);
+                }
+                statement.executeUpdate();    
             }
-            statement.executeUpdate();
         }
         conn.close();
     }
