54576f1bd0ce3ec01ebb687964c81f04d60f9e77
Bruno Codeman <brunobemfica@gmail.com>
2023-10-04 22:51:50 -0300

    implementando tela de serviço central

--- src/main/java/br/org/cria/splinkerapp/controllers/CentralServiceController.java
+++ src/main/java/br/org/cria/splinkerapp/controllers/CentralServiceController.java
@@ -1,11 +1,17 @@
 package br.org.cria.splinkerapp.controllers;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import br.org.cria.splinkerapp.config.BaseConfiguration;
+import br.org.cria.splinkerapp.models.CentralService;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 
-public class CentralServiceController extends AbstractController{
+public class CentralServiceController extends AbstractController implements Initializable {
 
     @FXML
     Pane pane;
@@ -19,8 +25,35 @@ public class CentralServiceController extends AbstractController{
     TextField uriField;
     
     @FXML
-    void onButtonSavedClick(){
+    void onButtonSavedClick() 
+    {
+       try 
+       {
+            var cserv = new CentralService(uriField.getText(),urlField.getText());
+            BaseConfiguration.saveCentralServiceData(cserv); 
+       } 
+       catch (Exception e) 
+       {
+            showErrorModal(e.getMessage());
+       }
+    }
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) 
+    {
+        try 
+        {
+            var centralServiceConfig = BaseConfiguration.getCentraServiceData();
+            if(centralServiceConfig != null)
+            {
+                uriField.setText(centralServiceConfig.getCentralServiceUri());
+                urlField.setText(centralServiceConfig.getCentralServiceUrl());
+            }    
+        } 
+        catch (Exception e) 
+        {
+            showErrorModal(e.getMessage());
+        }    
     }
     
 }
