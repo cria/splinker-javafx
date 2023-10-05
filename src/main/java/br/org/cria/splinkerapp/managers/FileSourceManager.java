179410562563dd8c07cb5b66565a18431d29be56
Bruno Codeman <brunobemfica@gmail.com>
2023-10-04 22:57:18 -0300

    Adicionando Tratamento para formato não suportado

--- src/main/java/br/org/cria/splinkerapp/managers/FileSourceManager.java
+++ src/main/java/br/org/cria/splinkerapp/managers/FileSourceManager.java
@@ -1,8 +1,5 @@
 package br.org.cria.splinkerapp.managers;
 
-import java.time.Duration;
-import java.time.Instant;
-
 import br.org.cria.splinkerapp.models.DataSource;
 import br.org.cria.splinkerapp.models.DataSourceType;
 import br.org.cria.splinkerapp.parsers.CsvFileParser;
@@ -12,7 +9,6 @@ import br.org.cria.splinkerapp.parsers.FileParser;
 import br.org.cria.splinkerapp.parsers.OdsFileParser;
 import br.org.cria.splinkerapp.services.implementations.DarwinCoreArchiveService;
 import javafx.concurrent.Service;
-import javafx.concurrent.Task;
 
 public class FileSourceManager {
 
@@ -22,13 +18,17 @@ public class FileSourceManager {
             
                         FileParser fileParser = null;
                         DataSourceType type = null;
+                        var dwcManager = new DarwinCoreArchiveService();
                         var isExcel = filePath.endsWith(".xlsx") ||filePath.endsWith(".xls");
                         var isCsv = filePath.endsWith(".csv");
                         var isOds = filePath.endsWith(".ods");
                         var isDbf = filePath.endsWith(".dbf");
-                        var start = Instant.now();
-                        System.out.println("starting at " + start);
-                        
+                        var unsupportedFileFormat = isExcel || isCsv || isOds || isDbf;
+
+                        if(unsupportedFileFormat)
+                        {
+                            throw new Exception("Formato de arquivo não suportado");
+                        }
                         if(isExcel)
                         {
                             fileParser = new ExcelFileParser(filePath);
@@ -52,29 +52,12 @@ public class FileSourceManager {
                        
                         fileParser.createTableBasedOnSheet();
                         fileParser.insertDataIntoTable();
-                        var end = Instant.now();
-                        var duration = Duration.between(start,end);
-                        System.out.println("Duration in seconds: %s".formatted(duration.getSeconds()));
-                        var dwcManager = new DarwinCoreArchiveService();
+                        
+                        
                         dwcManager.readDataFromSource(new DataSource(type))
                         .generateTXTFile()
                         .generateZIPFile();
                         return dwcManager.transferData();
-                        //return null;
-
-        // return new Service<Void>() {
-        //     @Override
-        //     protected Task<Void> createTask()
-        //     {
-        //         return new Task<>() {
-        //             @Override
-        //             protected Void call() throws Exception
-        //             {
-                    
-        //             }
-        //         };
-        //     }
-        //};
 
     }
     
