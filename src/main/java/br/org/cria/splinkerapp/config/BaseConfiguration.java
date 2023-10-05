1a5cacc7290c3b5e97088bdc7dc097af7db2b00c
Bruno Codeman <brunobemfica@gmail.com>
2023-10-04 22:51:32 -0300

    implementando serviço de configuração básica

--- src/main/java/br/org/cria/splinkerapp/config/BaseConfiguration.java
+++ src/main/java/br/org/cria/splinkerapp/config/BaseConfiguration.java
@@ -2,6 +2,7 @@ package br.org.cria.splinkerapp.config;
 
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import br.org.cria.splinkerapp.models.CentralService;
 
 public class BaseConfiguration {
     public static String LOCAL_DB_CONNECTION = "jdbc:sqlite:splinker.db";
@@ -9,8 +10,8 @@ public class BaseConfiguration {
     {
         var cmd = "INSERT INTO BasicConfiguration (token) VALUES(?)";
         var statement = DriverManager.getConnection(LOCAL_DB_CONNECTION).prepareStatement(cmd);
-        statement.setString(0, token);
-        statement.executeQuery();
+        statement.setString(1, token);
+        statement.executeUpdate();
     }
     public static String getToken() throws Exception
     {
@@ -29,6 +30,28 @@ public class BaseConfiguration {
         var conn = DriverManager.getConnection(LOCAL_DB_CONNECTION);
         return conn.createStatement().executeQuery(sql);
     }
+    public static CentralService getCentraServiceData() throws Exception
+    {
+        var cmd = "SELECT * FROM CentralServiceConfiguration";
+        var result = runQuery(cmd);
+        var uri = result.getString("central_service_uri");
+        var url = result.getString("central_service_uri");
+        return new CentralService(uri, url);
+    };
+
+    public static void saveCentralServiceData(CentralService cserv) throws Exception
+    {
+        var cmd = """
+                    INSERT INTO CentralServiceConfiguration (central_service_uri, central_service_url) 
+                    VALUES(?,?)
+                    """;
+        var statement = DriverManager.getConnection(LOCAL_DB_CONNECTION).prepareStatement(cmd);
+        statement.setString(1, cserv.getCentralServiceUri());
+        statement.setString(2, cserv.getCentralServiceUrl());
+        statement.executeUpdate();
+    }
+    
+
 
     
 }
