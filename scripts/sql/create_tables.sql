57a6f4780fe43f48eafb429a07d5f5e1e438fe5c
Bruno Codeman <brunobemfica@gmail.com>
2023-10-07 23:34:51 -0300

    Melhorando disposição dos dados no BD local

--- scripts/sql/create_tables.sql
+++ scripts/sql/create_tables.sql
@@ -1,26 +1,5 @@
 CREATE TABLE IF NOT EXISTS BasicConfiguration (
   token VARCHAR(50) NOT NULL,
-  datasource_filepath VARCHAR(50) NOT NULL,
-  created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
-  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS ProxyConfiguration (
-  proxy_username VARCHAR(50) NOT NULL,
-  proxy_password VARCHAR(100) NOT NULL,
-  proxy_port VARCHAR(10) NOT NULL,
-  proxy_address VARCHAR(100) NOT NULL,
-  created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
-  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS DbConfiguration (
-  db_host VARCHAR(100) NOT NULL,
-  db_port VARCHAR(100) NOT NULL,
-  db_name VARCHAR(100) NOT NULL,
-  db_tablename VARCHAR(100) NOT NULL,
-  db_username VARCHAR(100) NOT NULL,
-  db_password VARCHAR(100) NOT NULL,
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
   updated_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
@@ -39,5 +18,30 @@ CREATE TABLE IF NOT EXISTS TransferConfiguration (
   updated_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS ProxyConfiguration (
+  proxy_username VARCHAR(50) NOT NULL,
+  proxy_password VARCHAR(100) NOT NULL,
+  proxy_port VARCHAR(10) NOT NULL,
+  proxy_address VARCHAR(100) NOT NULL,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS DataSourceConfiguration (
+  datasource_filepath VARCHAR(50) NULL,
+  datasource_type VARCHAR(50) NOT NULL,
+  db_host VARCHAR(100) NOT NULL,
+  db_port VARCHAR(100) NOT NULL,
+  db_name VARCHAR(100) NOT NULL,
+  db_tablename VARCHAR(100) NOT NULL,
+  db_username VARCHAR(100) NOT NULL,
+  db_password VARCHAR(100) NOT NULL,  
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+
 INSERT INTO TransferConfiguration (rsync_port, rsync_server_destination)
 VALUES (10000, 'bruno@35.224.172.146::meu_modulo');
+
+INSERT INTO CentralServiceConfiguration (central_service_uri, central_service_url)
+VALUES ('spLinker.com', 'https://splinker.cria.org.br');
