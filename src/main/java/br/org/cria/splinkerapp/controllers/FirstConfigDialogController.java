541d2e84b1f503e5ed03073bc5bcfdbdd0e85394
Bruno Codeman <brunobemfica@gmail.com>
2023-10-08 00:35:05 -0300

    continuando fluxo da aplicação

--- src/main/java/br/org/cria/splinkerapp/controllers/FirstConfigDialogController.java
+++ src/main/java/br/org/cria/splinkerapp/controllers/FirstConfigDialogController.java
@@ -1,6 +1,5 @@
 package br.org.cria.splinkerapp.controllers;
-import br.org.cria.splinkerapp.Router;
-import br.org.cria.splinkerapp.services.implementations.ProxyConfigService;
+import br.org.cria.splinkerapp.repositories.ProxyConfigRepository;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
@@ -15,24 +14,22 @@ public class FirstConfigDialogController extends AbstractController{
     @FXML
     Button btnYes;
 
-    ProxyConfigService proxyService = new ProxyConfigService();
-
     @FXML
-    void onYesButtonClicked(){
+    void onYesButtonClicked()
+    {
         var routeName = "central-service";
         var width = 350;
         var height = 200;
         var stage = getStage();
-        var computerHasProxyConfigured = ProxyConfigService.isBehindProxyServer();
+        var computerHasProxyConfigured = ProxyConfigRepository.isBehindProxyServer();
+        
         if(computerHasProxyConfigured)
         {
             routeName = "proxy-config";
             width = 440;
             height = 400;
-            stage = getStage(); 
         }
-        
-        Router.getInstance().navigateTo(stage,routeName, width, height);
+        navigateTo(stage,routeName, width, height);
     }
 
     @FXML
