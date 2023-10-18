6a3c73641c1076f14d1e5d2cdeb1b318fece52ad
Bruno Codeman <brunobemfica@gmail.com>
2023-10-17 23:39:20 -0300

    mudanças cosméticas

--- src/main/java/br/org/cria/splinkerapp/services/BaseService.java
+++ src/main/java/br/org/cria/splinkerapp/services/BaseService.java
@@ -6,9 +6,9 @@ import java.sql.SQLException;
 
 public abstract class BaseService {
     
-protected static Connection getConnection() throws SQLException {
-    String url = "jdbc:sqlite:splinker.db";
-    return DriverManager.getConnection(url);
-}
-
+    protected static Connection getConnection() throws SQLException 
+    {
+        String url = "jdbc:sqlite:splinker.db";
+        return DriverManager.getConnection(url);
+    }
 }
