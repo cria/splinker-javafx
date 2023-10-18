19ab5804a96ef63768b4a3c4d654144b00293aba
Bruno Codeman <brunobemfica@gmail.com>
2023-10-17 23:28:54 -0300

    implementando navegação e leitura de configuração no login

--- src/main/java/br/org/cria/splinkerapp/controllers/TokenLoginController.java
+++ src/main/java/br/org/cria/splinkerapp/controllers/TokenLoginController.java
@@ -2,7 +2,9 @@ package br.org.cria.splinkerapp.controllers;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-
+import br.org.cria.splinkerapp.facade.ConfigFacade;
+import br.org.cria.splinkerapp.models.DataSourceType;
+import br.org.cria.splinkerapp.repositories.DataSourceRepository;
 import br.org.cria.splinkerapp.repositories.TokenRepository;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -27,12 +29,37 @@ public class TokenLoginController extends AbstractController implements Initiali
         try 
         {
             var token = tokenField.getText();
-            var isTokenValid = TokenRepository.validateToken(token);
-            if(isTokenValid)
+            var apiConfig = TokenRepository.getConfigurationDataFromAPI(token);
+            if(apiConfig != null)
             {
                 TokenRepository.saveToken(token);
-                navigateTo(getStage(), "datasource-selection",400,300);
-                return;   
+                ConfigFacade.handleConfiguration(apiConfig);
+                var dsType = DataSourceType.valueOf(apiConfig.get("data_source_type").toString());
+                DataSourceRepository.saveDataSource(dsType);
+                var routeName = "collection-database";
+                var width = 255;
+                var height = 360;
+                switch(dsType)
+                {
+                    case Access:
+                        routeName = "access-db-modal";
+                        width = 424;
+                        height = 158;
+                        break;
+                    case dBase:
+                    case Excel:
+                    case LibreOfficeCalc:
+                    case CSV:
+                        routeName = "";
+                        width = 369;
+                        height = 127;
+                        break;
+                    default:
+                        break;
+                }
+        
+                navigateTo(getStage(), routeName, width, height);
+                return;
             }
                 showAlert(AlertType.ERROR,"Token inválido", "O token digitado é inválido");
              
