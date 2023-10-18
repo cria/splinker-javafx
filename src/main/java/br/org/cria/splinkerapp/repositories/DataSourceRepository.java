e9dddbdf654d6bcd80e17e96bf585135e9741085
Bruno Codeman <brunobemfica@gmail.com>
2023-10-17 23:38:14 -0300

    salvando data source e comando sql

--- src/main/java/br/org/cria/splinkerapp/repositories/DataSourceRepository.java
+++ src/main/java/br/org/cria/splinkerapp/repositories/DataSourceRepository.java
@@ -1,38 +1,46 @@
 package br.org.cria.splinkerapp.repositories;
 
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.sql.DriverManager;
+import java.util.List;
 
 import br.org.cria.splinkerapp.models.DataSource;
 import br.org.cria.splinkerapp.models.DataSourceType;
 
-public class DataSourceRepository extends BaseRepository{
+public class DataSourceRepository extends BaseRepository {
+
+    public static boolean checkIfRecordsHaveDecreased()
+    {
+        /*
+            var dsCount = *Executa o select salvo*;
+            var fileLineCount = Files.lines(path).count() - 1; //header
+            return fileLineCount >
+         * 
+        */
+        return false;
+    }
 
     public static DataSource getDataSource() throws Exception
     {
-        var cmd = """
-                SELECT datasource_filepath, datasource_type, db_host,
-                db_port, db_name, db_tablename,
-                db_username, db_password
-                FROM DataSourceConfiguration
-                LIMIT 1;
-                """;
+        var cmd = "SELECT * FROM DataSourceConfiguration;";
         var conn = DriverManager.getConnection(LOCAL_DB_CONNECTION);
         var result = runQuery(cmd, conn);
         DataSource ds = null;
 
         while(result.next())
         {
-            var filePath = result.getString(1);
-            var type = DataSourceType.valueOf(result.getString(2));
+            var filePath = result.getString("datasource_filepath");
+            var type = DataSourceType.valueOf(result.getString("datasource_type"));
             if(filePath == null)
             {
-                var host = result.getString(3);
-                var port = result.getString(4);
-                var dbName = result.getString(5);
-                var table = result.getString(6);
-                var user = result.getString(7);
-                var pwd = result.getString(8);
+                var host = result.getString("db_host");
+                var port = result.getString("db_port");
+                var dbName = result.getString("db_name");
+                var table = result.getString("db_tablename");
+                var user = result.getString("db_username");
+                var pwd = result.getString("db_password");
                 ds = new DataSource(type, host, dbName, table, user, pwd, port);
             }
             else
@@ -44,7 +52,33 @@ public class DataSourceRepository extends BaseRepository{
         return ds;
     }
 
-    public void saveDataSource(DataSourceType type, String host, String port, 
+    public static void saveSQLCommand(List<Double> cmd) throws Exception
+    {
+        var fileName = "%s/sql_command.sql".formatted(System.getProperty("user.dir"));
+        var sqlCmd = byteArrayToString(cmd);
+        Path path = Paths.get(fileName);
+        byte[] strToBytes = sqlCmd.getBytes();
+        if(Files.exists(path))
+        {
+            Files.delete(path);
+        }
+        Files.write(path, strToBytes);
+    }
+
+    public static String getSQLCommand() throws Exception 
+    {
+        var fileName = "%s/sql_command.sql".formatted(System.getProperty("user.dir"));
+        String read = String.join("",Files.readAllLines(Path.of(fileName)));
+        return read;
+    }
+
+
+    public static void saveDataSource(DataSourceType type) throws Exception
+    {
+        saveDataSource(type, null, null, null,null,null,null);
+    }
+
+    public static void saveDataSource(DataSourceType type, String host, String port, 
                             String dbName, String table, String user, String password) throws Exception
     {
         cleanTable("DataSourceConfiguration");
