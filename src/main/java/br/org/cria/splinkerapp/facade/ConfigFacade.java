81baa9d2871b835dfa860f6d67cb42aa37e918e7
Bruno Codeman <brunobemfica@gmail.com>
2023-10-17 23:29:48 -0300

    implementando façade de gestão de configuração

---
+++ src/main/java/br/org/cria/splinkerapp/facade/ConfigFacade.java
@@ -0,0 +1,23 @@
+package br.org.cria.splinkerapp.facade;
+
+import java.util.List;
+import java.util.Map;
+import br.org.cria.splinkerapp.repositories.CentralServiceRepository;
+import br.org.cria.splinkerapp.repositories.DataSourceRepository;
+import br.org.cria.splinkerapp.repositories.TransferConfigRepository;
+
+public class ConfigFacade {
+
+    public static void handleConfiguration(Map apiConfig) throws Exception
+    {
+        var cmd = (List<Double>) apiConfig.get("sql_command");
+        var rsyncPort = (Double) apiConfig.get("rsync_port");
+        var rsyncDestination = apiConfig.get("rsync_destination").toString();
+        var centralServiceUri = apiConfig.get("central_service_uri").toString();
+        var centralServiceUrl = apiConfig.get("central_service_url").toString();
+        DataSourceRepository.saveSQLCommand(cmd);
+        TransferConfigRepository.saveRSyncConfig(rsyncPort.intValue(), rsyncDestination);
+        CentralServiceRepository.saveCentralServiceData(centralServiceUri, centralServiceUrl);
+    }
+    
+}
