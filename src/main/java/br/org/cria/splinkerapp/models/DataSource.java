e450bdfc3a68fe2adcc1e6dc29b6eec2267a4721
Bruno Codeman <brunobemfica@gmail.com>
2023-10-17 23:34:20 -0300

    Implementando lógica de fonte de dados

--- src/main/java/br/org/cria/splinkerapp/models/DataSource.java
+++ src/main/java/br/org/cria/splinkerapp/models/DataSource.java
@@ -3,18 +3,82 @@ package br.org.cria.splinkerapp.models;
 import java.nio.file.Path;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.time.LocalDate;
 
 public class DataSource {
+    private DataSourceType type;
+    private Connection connection;
     private String connectionString = "jdbc:sqlite:splinker.db";
+    private String dataSourceFilePath;
+    public String getDataSourceFilePath() {
+        return dataSourceFilePath;
+    }
+    public void setDataSourceFilePath(String dataSourceFilePath) {
+        this.dataSourceFilePath = dataSourceFilePath;
+    }
+
+    private String dbHost;
+    public String getDbHost() {
+        return dbHost;
+    }
+    public void setDbHost(String dbHost) {
+        this.dbHost = dbHost;
+    }
+
+    private String dbPort;
+    public String getDbPort() {
+        return dbPort;
+    }
+    public void setDbPort(String dbPort) {
+        this.dbPort = dbPort;
+    }
+
+    private String dbName;
+    public String getDbName() {
+        return dbName;
+    }
+    public void setDbName(String dbName) {
+        this.dbName = dbName;
+    }
+
+    private String dbTableName;
+    public String getDbTableName() {
+        return dbTableName;
+    }
+    public void setDbTableName(String dbTableName) {
+        this.dbTableName = dbTableName;
+    }
+
+    private String dbUser;
+    public String getDbUser() {
+        return dbUser;
+    }
+    public void setDbUser(String dbUser) {
+        this.dbUser = dbUser;
+    }
+
+    private String dbPassword;
+    public String getDbPassword() {
+        return dbPassword;
+    }
+    public void setDbPassword(String dbPassword) {
+        this.dbPassword = dbPassword;
+    }
+
+    private LocalDate createdAt;
+    public LocalDate getCreatedAt() {
+        return createdAt;
+    }
+    public void setCreatedAt(LocalDate createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    private LocalDate updatedAt;
+  
     private Path filePath;
     public Path getFilePath() {
         return filePath;
     }
-
-    private DataSourceType type;
-    private Connection connection;
-
-    
     public DataSource(DataSourceType type) { this.type = type; };
     public DataSource(DataSourceType type, Path filepath) { this.type = type; this.filePath = filepath; };
     public DataSource(DataSourceType type, Connection conn) 
