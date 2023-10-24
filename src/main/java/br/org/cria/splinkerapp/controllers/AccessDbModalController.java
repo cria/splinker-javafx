1467fc0b203fe7848055f31a2f932e50bc6e3f3e
Bruno Codeman <brunobemfica@gmail.com>
2023-10-24 00:17:13 -0300

    Modificando para edição de dados

--- src/main/java/br/org/cria/splinkerapp/controllers/AccessDbModalController.java
+++ src/main/java/br/org/cria/splinkerapp/controllers/AccessDbModalController.java
@@ -1,52 +1,99 @@
 package br.org.cria.splinkerapp.controllers;
 
 import java.io.File;
-
-import br.org.cria.splinkerapp.managers.DatabaseSourceManager;
+import java.net.URL;
+import java.util.ResourceBundle;
+import br.org.cria.splinkerapp.models.DataSourceType;
+import br.org.cria.splinkerapp.repositories.DataSourceRepository;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 import javafx.stage.FileChooser;
 
-public class AccessDbModalController extends AbstractController {
+public class AccessDbModalController extends AbstractController  implements Initializable {
     @FXML
     Pane pane;
     @FXML
     TextField accessUsernameField;
     @FXML
-    TextField accessPasswordField;
+    PasswordField accessPasswordField;
     @FXML
     TextField accessFilePathField;
     @FXML
     Button btnSave;
     @FXML
     Button btnSelectFile;
+    File file;
     FileChooser fileChooser = new FileChooser();
     @Override
     protected Pane getPane() {
         return pane;
     }
+    @FXML
+    void onBtnSaveClicked()
+    {
+        try
+        {
+            var path = file.getAbsolutePath();
+            var userName = accessUsernameField.getText();
+            var password = accessPasswordField.getText();
+            DataSourceRepository.saveDataSource(DataSourceType.Access, path, userName, password);
+            var routeName ="home";
+            var width = 231;
+            var height = 222;
+            navigateTo(getStage(), routeName, width, height);
+        } 
+        catch(Exception ex)
+        {
+            showErrorModal(ex.getMessage());
+        }
+    }
 
     @FXML
     void onBtnSelectFileClick()
     {
-        File file = fileChooser.showOpenDialog(getStage());
+        file = fileChooser.showOpenDialog(getStage());
         if (file != null) 
         {
             accessFilePathField.setText(file.getAbsolutePath());
-            try
-            {
-                var path = file.getAbsolutePath();
-                var userName = accessUsernameField.getText();
-                var password = accessPasswordField.getText();
-                transferService = DatabaseSourceManager.processData(path, userName, password);
-            } 
-            catch(Exception ex)
-            {
-                showErrorModal(ex.getMessage());
-            }
+            btnSave.setDisable(false);    
         }
+    }
+    @Override
+    public void initialize(URL location, ResourceBundle resources) 
+    {
+        try 
+        {
+            var ds = DataSourceRepository.getDataSource();
+            if(ds!= null)
+            {
+                var hasUserName = ds.getDbUser() != null;
+                var hasPassword = ds.getDbPassword() != null;
+                var hasFilePath = ds.getDataSourceFilePath() != null;
+                if(hasFilePath)
+                {
+                    accessFilePathField.setText(ds.getDataSourceFilePath());
+                }
+                if(hasUserName)
+                {
+                    accessUsernameField.setText(ds.getDbUser());
+                }
+                if(hasPassword)
+                {
+                    accessPasswordField.setText(ds.getDbPassword());
+                }
+            }
+        } 
+        catch (Exception e) 
+        {
+            showErrorModal(e.getMessage());
+        }
+        
+        
+        btnSave.setDisable(true);    
     }
     
 }
