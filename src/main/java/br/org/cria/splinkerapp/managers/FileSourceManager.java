6ef986bcce3b6ae92ba1b494c2f5951e6f9d9490
Bruno Codeman <brunobemfica@gmail.com>
2023-10-29 22:19:38 -0300

    alterando lógica de processamento de fonte de dados

--- src/main/java/br/org/cria/splinkerapp/managers/FileSourceManager.java
+++ src/main/java/br/org/cria/splinkerapp/managers/FileSourceManager.java
@@ -1,12 +1,11 @@
 package br.org.cria.splinkerapp.managers;
 
-import br.org.cria.splinkerapp.models.DataSource;
-import br.org.cria.splinkerapp.models.DataSourceType;
 import br.org.cria.splinkerapp.parsers.CsvFileParser;
 import br.org.cria.splinkerapp.parsers.DbfFileParser;
 import br.org.cria.splinkerapp.parsers.ExcelFileParser;
 import br.org.cria.splinkerapp.parsers.FileParser;
 import br.org.cria.splinkerapp.parsers.OdsFileParser;
+import br.org.cria.splinkerapp.repositories.DataSourceRepository;
 import br.org.cria.splinkerapp.services.implementations.DarwinCoreArchiveService;
 import javafx.concurrent.Service;
 
@@ -16,13 +15,12 @@ public class FileSourceManager {
     {
         var filePath = fileToProcess.toLowerCase();
         FileParser fileParser = null;
-        DataSourceType type = null;
         var dwcManager = new DarwinCoreArchiveService();
         var isExcel = filePath.endsWith(".xlsx") ||filePath.endsWith(".xls");
         var isCsv = filePath.endsWith(".csv");
         var isOds = filePath.endsWith(".ods");
         var isDbf = filePath.endsWith(".dbf");
-        var unsupportedFileFormat = isExcel || isCsv || isOds || isDbf;
+        var unsupportedFileFormat = !(isExcel || isCsv || isOds || isDbf);
 
         if(unsupportedFileFormat)
         {
@@ -31,28 +29,24 @@ public class FileSourceManager {
         if(isExcel)
         {
             fileParser = new ExcelFileParser(filePath);
-            type = DataSourceType.Excel;
         }
         if(isCsv)
         {
             fileParser = new CsvFileParser(filePath);
-            type = DataSourceType.CSV;
         }
         if(isOds)
         {
             fileParser = new OdsFileParser(filePath);
-            type = DataSourceType.LibreOfficeCalc;
         }
         if(isDbf)
         {
             fileParser = new DbfFileParser(filePath);
-            type = DataSourceType.dBase;
         }
 
         fileParser.createTableBasedOnSheet();
         fileParser.insertDataIntoTable();
-
-        dwcManager.readDataFromSource(new DataSource(type))
+        var ds = DataSourceRepository.getDataSource();
+        dwcManager.readDataFromSource(ds)
         .generateTXTFile()
         .generateZIPFile();
         return dwcManager.transferData();
