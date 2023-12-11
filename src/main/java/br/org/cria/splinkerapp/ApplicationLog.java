32bc6d5a217f64ee7917f619aacee9a636abdedc
Bruno Codeman <brunobemfica@gmail.com>
2023-12-11 01:21:57 -0300

    criando classe de logging

---
+++ src/main/java/br/org/cria/splinkerapp/ApplicationLog.java
@@ -0,0 +1,27 @@
+package br.org.cria.splinkerapp;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class ApplicationLog {
+    protected static final Logger logger = LogManager.getLogger(ApplicationLog.class);
+    public static void error(String erroMessage)
+    {
+        logger.error(erroMessage);
+    }
+
+    public static void info(String warningMessage)
+    {
+        logger.info(warningMessage);
+    }
+
+    public static void debug(String message)
+    {
+        logger.debug(message);
+    }
+
+    public static void warn(String message)
+    {
+        logger.warn(message);
+    }
+}
