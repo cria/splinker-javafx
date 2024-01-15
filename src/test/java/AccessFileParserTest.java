1d4bdac5e5bc03dcc9a871969d8d78fecd111bba
Bruno Codeman <brunobemfica@gmail.com>
2024-01-15 18:34:32 -0300

    testando execução de testes no actions

--- src/test/java/AccessFileParserTest.java
+++ src/test/java/AccessFileParserTest.java
@@ -23,7 +23,7 @@ public class AccessFileParserTest extends ParserBaseTest {
     static List<FileFormat> unsupportedFormats = Arrays.asList(FileFormat.GENERIC_JET4, FileFormat.V1997, FileFormat.MSISAM);
     static List<FileFormat> fileformats = Arrays.asList(FileFormat.values()).stream()
             .filter(e -> !unsupportedFormats.contains(e)).toList();
-    static int numberOfRows = (rowCount/3)+12;
+    static int numberOfRows = (rowCount/3);
     @Test
     public void parseV2000AccessFileTest() throws Exception {
 
