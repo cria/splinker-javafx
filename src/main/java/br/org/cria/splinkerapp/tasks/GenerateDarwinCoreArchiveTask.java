4bd7d75c651abbc67d64dc984a90227fdce01b68
Bruno Codeman <brunobemfica@gmail.com>
2024-01-14 23:51:30 -0300

    Adicionando tasks

--- src/main/java/br/org/cria/splinkerapp/tasks/GenerateDarwinCoreArchiveTask.java
+++ src/main/java/br/org/cria/splinkerapp/tasks/GenerateDarwinCoreArchiveTask.java
@@ -1,18 +1,43 @@
 package br.org.cria.splinkerapp.tasks;
 
+import java.time.Instant;
+import java.time.ZoneId;
+
+import com.google.common.eventbus.EventBus;
+import com.google.common.eventbus.Subscribe;
+
+import br.org.cria.splinkerapp.ApplicationLog;
+import br.org.cria.splinkerapp.enums.EventTypes;
+import br.org.cria.splinkerapp.managers.EventBusManager;
 import br.org.cria.splinkerapp.services.implementations.DarwinCoreArchiveService;
 import javafx.concurrent.Task;
 
 public class GenerateDarwinCoreArchiveTask extends Task<DarwinCoreArchiveService> {
     DarwinCoreArchiveService service;
+    EventBus writeRowEventBus;
 
     public GenerateDarwinCoreArchiveTask(DarwinCoreArchiveService service)
     {
         this.service = service;
+        writeRowEventBus = EventBusManager.getEvent(EventTypes.WRITE_ROW.name());
+        writeRowEventBus.register(this);
     }
+
+    @Subscribe
+    void onWriteRow(Integer rowCount)
+    {
+        var totalRowCount = service.getTotalRowCount();
+        updateProgress(rowCount, totalRowCount);// manager.getParser().getTotalRowCount());
+    }
+
 
     @Override
     protected DarwinCoreArchiveService call() throws Exception {
+        var now = Instant.now().atZone(ZoneId.systemDefault());
+        var message = now.toLocalDateTime() + " - Iniciando a criação do arquivo DWC";
+        
+        ApplicationLog.info(message);
+        
         return service.readDataFromSource()
                       .generateTXTFile()
                       .generateZIPFile();
--- src/main/java/br/org/cria/splinkerapp/tasks/ImportDataTask.java
+++ src/main/java/br/org/cria/splinkerapp/tasks/ImportDataTask.java
@@ -2,26 +2,42 @@ package br.org.cria.splinkerapp.tasks;
 
 import java.time.Instant;
 import java.time.ZoneId;
+import com.google.common.eventbus.EventBus;
+import com.google.common.eventbus.Subscribe;
 import br.org.cria.splinkerapp.ApplicationLog;
+import br.org.cria.splinkerapp.enums.EventTypes;
+import br.org.cria.splinkerapp.managers.EventBusManager;
 import br.org.cria.splinkerapp.managers.FileSourceManager;
 import br.org.cria.splinkerapp.models.DataSet;
 import javafx.concurrent.Task;
 
 public class ImportDataTask extends Task<Void> {
-
     DataSet ds;
-    public ImportDataTask(DataSet ds)
+    EventBus readRowEventBus = EventBusManager.getEvent(EventTypes.READ_ROW.name());
+    FileSourceManager manager;
+    
+    public ImportDataTask(DataSet ds) throws Exception
     {
         this.ds = ds;
+        manager = new FileSourceManager(ds);
+        readRowEventBus.register(this);
+    }
+
+    @Subscribe
+    void onRowCountUpdate(Integer rowCount)
+    {
+        var totalRowCount = manager.getParser().getTotalRowCount();
+        updateProgress(rowCount, totalRowCount);
     }
 
     @Override
-    protected Void call() throws Exception {
+    protected Void call() throws Exception 
+    {
         var now = Instant.now().atZone(ZoneId.systemDefault());
-        var message = now.toLocalDateTime() + " - Iniciando a importação do arquivo";
+        var message = "%s - Iniciando a importação do arquivo".formatted(now.toLocalDateTime());
         ApplicationLog.info(message);
-        FileSourceManager.importData(null);
+        System.out.println(message);
+        manager.importData();
         return null;
     }
-    
 }
