ed7a2fec55e87282996a720c6b59b1737b8b179a
Bruno Bemfica <bruno.bemfica@udemy.com>
2024-02-29 20:54:52 -0300

    Melhorando design da aplicação

--- src/main/java/br/org/cria/splinkerapp/enums/WindowSizes.java
+++ src/main/java/br/org/cria/splinkerapp/enums/WindowSizes.java
@@ -1,12 +1,12 @@
 package br.org.cria.splinkerapp.enums;
 
 public class WindowSizes {    
-    public final static int SMALL_SQUARE_SCREEN_WIDTH = 250;
-    public final static int SMALL_SQUARE_SCREEN_HEIGHT = 250;
-    public final static int SMALL_RECTANGULAR_SCREEN_WIDTH = 250;
-    public final static int SMALL_RECTANGULAR_SCREEN_HEIGHT = 200;
-    public final static int LARGE_SQUARE_SCREEN_WIDTH = 360;
-    public final static int LARGE_SQUARE_SCREEN_HEIGHT = 360;
-    public final static int LARGE_RECTANGULAR_SCREEN_WIDTH = 360;
-    public final static int LARGE_RECTANGULAR_SCREEN_HEIGHT = 300;
+    public final static int SMALL_SQUARE_SCREEN_WIDTH = 300;
+    public final static int SMALL_SQUARE_SCREEN_HEIGHT = 300;
+    public final static int SMALL_RECTANGULAR_SCREEN_WIDTH = 350;
+    public final static int SMALL_RECTANGULAR_SCREEN_HEIGHT = 300;
+    public final static int LARGE_SQUARE_SCREEN_WIDTH = 420;
+    public final static int LARGE_SQUARE_SCREEN_HEIGHT = 420;
+    public final static int LARGE_RECTANGULAR_SCREEN_WIDTH = 420;
+    public final static int LARGE_RECTANGULAR_SCREEN_HEIGHT = 400;
 }
