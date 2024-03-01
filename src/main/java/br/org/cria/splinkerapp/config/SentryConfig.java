fb1f095ab2380d6f145d7b3828330ade903bad04
Bruno Bemfica <bruno.bemfica@udemy.com>
2024-03-01 02:59:20 -0300

    adicionando checagem de proxy ao Sentry

--- src/main/java/br/org/cria/splinkerapp/config/SentryConfig.java
+++ src/main/java/br/org/cria/splinkerapp/config/SentryConfig.java
@@ -1,11 +1,39 @@
 package br.org.cria.splinkerapp.config;
 
+import br.org.cria.splinkerapp.repositories.ProxyConfigRepository;
+import io.sentry.Sentry;
+import io.sentry.SentryOptions.Proxy;
+
 public class SentryConfig {
 
     public static void setUp()
     {
+        var dsn = "https://cf699813548043f5c734471240b2ec6c@o4506639520104448.ingest.sentry.io/4506639532032000";
         var sentryToken = "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";
         System.setProperty("SENTRY_AUTH_TOKEN", sentryToken);
+        Sentry.init(options -> {
+            options.setDsn(dsn);
+            options.setEnvironment("development");
+            if(ProxyConfigRepository.isBehindProxyServer())
+            {
+                try
+                {
+                    var proxyConfig = ProxyConfigRepository.getConfiguration();
+                    var proxyHost = proxyConfig.getAddress();
+                    var proxyPort = proxyConfig.getPort();
+                    var proxy = new Proxy(proxyHost, proxyPort);
+                    options.setProxy(proxy);
+                }
+                catch(Exception e)
+                {
+                    throw new RuntimeException(e);
+                }
+                
+            }
+            
+            options.setTracesSampleRate(1.0);
+          });
+          
     }
     
 }
