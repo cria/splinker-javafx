8dfdcf3ef2db202715203abbcf164b134795355b
Bruno Bemfica <bruno.bemfica@udemy.com>
2024-03-20 14:33:39 -0300

    corrigindo script de atualização powershell

--- src/main/resources/scripts/update/softwareUpdate.ps1
+++ src/main/resources/scripts/update/softwareUpdate.ps1
@@ -1,19 +1,21 @@
 param (
-    # height of largest column without top bar
+    # Link para download do spLinker
     [Parameter(Mandatory=$true)]
     [string]$DOWNLOAD_LINK,
     
-    # name of the output image
+    # Caminho onde o instalador será salvo
     [string]$OUTPUT_PATH
 )
 
-# Download the new version (replace 'download_link' with the actual link)
+# Faz Download da nova versão
 Invoke-WebRequest -Uri $DOWNLOAD_LINK -OutFile $OUTPUT_PATH
 
-# Extract the downloaded file (replace 'new_version.zip' with the actual file name)
-# Expand-Archive -Path new_version.zip -DestinationPath .
+# Extrai o zip do download
+# Expand-Archive -Path splinker_new_version.zip -DestinationPath .
+
+# Remove a versão antiga do spLinker
+$software = Get-WmiObject -Class Win32_Product | Where-Object {$_.Name -like "*spLinker*"} | Select-Object -Property Name, IdentifyingNumber
+msiexec.exe /x $software.IdentifyingNumber /quiet /qn /norestart
 
 # Run GUI installer
-$software = Get-WmiObject -Class Win32_Product | Where-Object {$_.Name -like '*spLinker*'}
-$software.Uninstall()
 Start-Process -FilePath $OUTPUT_PATH -Wait
