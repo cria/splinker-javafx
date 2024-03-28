7423a8d6f1a2a0f4bcff001c46b4b523fbc23fa8
Bruno Bemfica <bruno.bemfica@udemy.com>
2024-03-28 11:56:53 -0300

    comentando código momentaneamente para adicionar tipagem

--- src/main/java/br/org/cria/splinkerapp/services/implementations/SpLinkerUpdateService.java
+++ src/main/java/br/org/cria/splinkerapp/services/implementations/SpLinkerUpdateService.java
@@ -14,16 +14,23 @@ public class SpLinkerUpdateService
     static String params = "-Command";
     static String githubURL = "https://api.github.com/repos/cria/splinker-javafx/releases";
     
+    //1) faz request no github
+    //2) Parsing de string para verificar a versão
+    //3) verifica se tem versão nova disponível
+    //4) Pega a URL do download
+    //5) Roda o script
+
     public static boolean hasNewVersion() throws Exception
     {
         var needUpdates = false;
-        var strVersion = CentralServiceRepository.getCentralServiceData().getSystemVersion();
-        var currentVersion = Double.parseDouble(strVersion.replaceAll("[^0-9]", ""));
-        var response = HttpService.getJson(githubURL);
-        var releaseName = response.get("name").toString();
-        var lastVersion = getVersionNumber(releaseName);
+        // var strVersion = CentralServiceRepository.getCentralServiceData().getSystemVersion();
+        // //TODO: Remover os pontos dos números de versão, transformar em inteiro e comparar para ver quem é maior
+        // var currentVersion = Double.parseDouble(strVersion.replaceAll("[^0-9]", ""));
+        // var response = HttpService.getJson(githubURL);
+        // var releaseName = response.get("name").toString();
+        // var lastVersion = getVersionNumber(releaseName);
         
-        needUpdates = lastVersion > currentVersion;
+        // needUpdates = lastVersion > currentVersion;
         
 
         return needUpdates;
@@ -69,7 +76,23 @@ public class SpLinkerUpdateService
     {
         try 
         {
-        
+            var scriptWithExtension = "%s%s".formatted(scriptPath, fileExtension);
+            var resource = SpLinkerUpdateService.class.getResource(scriptWithExtension);
+            var resourcePath = resource.toURI().getPath();
+            var fullScriptPath = operatingSystem.contains("windows")? resourcePath.substring(1) :resourcePath;
+            var home = System.getProperty("user.home");
+            var outputPath = "%s/Downloads/splinker_new_version.msi".formatted(home);
+            var downloadLink = "https://swupdate.openvpn.net/downloads/connect/openvpn-connect-3.3.7.2979_signed.msi";
+
+            var processBuilder = new ProcessBuilder();
+            var values =  "%s \"%s\" \"%s\";".formatted(fullScriptPath, downloadLink, outputPath);
+            processBuilder.command(command, params, values);
+            processBuilder.inheritIO(); // Redirect PowerShell's input, output, and error streams to the Java application
+            
+            //processBuilder.environment().put("$env:DOWNLOAD_LINK", downloadUrl);
+            
+            Process shellProcess = processBuilder.start();
+            System.exit(0);
         } 
         catch (Exception e) 
         {
