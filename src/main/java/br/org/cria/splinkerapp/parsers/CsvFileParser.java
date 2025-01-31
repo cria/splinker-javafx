15ac584d9967d54bdb7838a12aff582017ad1f11
IrvayneMatheus <irvaynematheus@yahoo.com>
2025-01-30 22:44:33 -0300

    CSv lendo todos os separadpres

--- src/main/java/br/org/cria/splinkerapp/parsers/CsvFileParser.java
+++ src/main/java/br/org/cria/splinkerapp/parsers/CsvFileParser.java
@@ -1,10 +1,13 @@
 package br.org.cria.splinkerapp.parsers;
 
+import java.io.BufferedReader;
 import java.io.File;
 
 import com.univocity.parsers.csv.CsvParser;
 import com.univocity.parsers.csv.CsvParserSettings;
 
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
@@ -47,6 +50,7 @@ public class CsvFileParser extends FileParser {
 
     @Override
     public void insertDataIntoTable() throws Exception {
+        String separator = detectSeparator(filePath);
         var conn = getConnection();
         conn.setAutoCommit(false);
         var tableName = getTableName();
@@ -59,9 +63,9 @@ public class CsvFileParser extends FileParser {
 
         while (iterator.hasNext()) {
             var row = iterator.next();
+            String[] rowValues = row[0].split(separator, -1);
             for (int j = 0; j < columnCount; j++) {
-                var cell = row[j];
-                var value = getCellValue(cell);
+                var value = (j < rowValues.length) ? getCellValue(rowValues[j]) : null;
                 statement.setString(j + 1, value);
             }
             statement.addBatch();
@@ -80,5 +84,20 @@ public class CsvFileParser extends FileParser {
         statement.clearBatch();
         conn.setAutoCommit(true);
         conn.close();
+    }
+    public static String detectSeparator(String filePath) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String firstLine = reader.readLine();
+            if (firstLine != null) {
+                if (firstLine.contains("\t")) {
+                    return "\t";
+                } else if (firstLine.contains(",")) {
+                    return ",";
+                } else if (firstLine.contains(";")) {
+                    return ";";
+                }
+            }
+        }
+        throw new IOException("Não foi possível detectar o separador.");
     }
 }
