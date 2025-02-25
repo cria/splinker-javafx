6f398420c06f05af936d0b12f1b5f2c03469f257
irvaynematheus <irvaynematheus@yahoo.com>
2025-02-25 14:09:08 -0300

    tratamento de dados com quebra de linha

--- src/main/java/br/org/cria/splinkerapp/parsers/FileParser.java
+++ src/main/java/br/org/cria/splinkerapp/parsers/FileParser.java
@@ -47,7 +47,7 @@ public abstract class FileParser {
     protected String getCellValue(String cell) {
         var isNull = cell == null;
         var hasNullValue = isNull || cell.equalsIgnoreCase("null");
-        var value = hasNullValue ? "" : cell.replace("\r", " ").replace("\t", " ").trim();
+        var value = hasNullValue ? "" : cell.replace("\r", " ").replace("\t", " ").replace("\n", " ").trim();
         return value;
     }
 
