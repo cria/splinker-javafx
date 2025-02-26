2cd62eaa926bb3ab4efd0055257122a169a0f69d
IrvayneMatheus <irvaynematheus@yahoo.com>
2025-02-26 12:16:37 -0300

    EmailRepository

--- src/main/java/br/org/cria/splinkerapp/facade/ConfigFacade.java
+++ src/main/java/br/org/cria/splinkerapp/facade/ConfigFacade.java
@@ -5,7 +5,9 @@ import java.util.Map;
 
 import static br.org.cria.splinkerapp.repositories.BaseRepository.byteArrayToString;
 
+import br.org.cria.splinkerapp.models.EmailConfiguration;
 import br.org.cria.splinkerapp.repositories.CentralServiceRepository;
+import br.org.cria.splinkerapp.repositories.EmailRepository;
 import br.org.cria.splinkerapp.repositories.TransferConfigRepository;
 import br.org.cria.splinkerapp.services.implementations.DataSetService;
 
@@ -18,11 +20,16 @@ public class ConfigFacade {
         var centralServiceUrl = apiConfig.get("central_service_url").toString();
         var sentryTokenArray = (List<Double>) apiConfig.get("sentry_token");
         var sentryToken = byteArrayToString(sentryTokenArray);
+        var emailSend = apiConfig.get("contact_email_send");
+        var emailToken = apiConfig.get("contact_email_token");
+        var emailRecipients = apiConfig.get("contact_email_recipients");
 
         System.setProperty("SENTRY_AUTH_TOKEN", sentryToken);
         var systemVersion = 1.1;
         //DataSetService.saveSQLCommand(token, cmd);
         TransferConfigRepository.saveRSyncConfig(rsyncPort.intValue(), rsyncDestination);
         CentralServiceRepository.saveCentralServiceData(centralServiceUrl, String.valueOf(systemVersion));
+        EmailRepository.saveEmailData((String) emailSend, (String) emailToken, (String) emailRecipients);
     }
+
 }
